4febc9ff5c7fb7d08958fb13ced78944016d5ae3
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-08-18 15:01:54 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -49,4 +49,6 @@ drawFilledCircle(width/3, height/3, 50, 'brown');
 
 drawFilledCircle(width/1.5, height/3, 50, 'brown');
 
-drawFilledCircle(width/2, height/2, 20, 'black');
+drawFilledCircle(width/2, height/2, 15, 'black');
+
+drawFilledCircle(width/2, height/2, 100, 'brown');
