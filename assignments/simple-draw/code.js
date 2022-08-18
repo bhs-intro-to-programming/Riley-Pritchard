d9a6b3cb3ee121b29a9cbc3b91f5f9ac53790c41
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-08-18 15:16:26 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -36,7 +36,7 @@
  */
 
 //drawLine(0, 0, width, height, 'red')
-drawFilledRect(0, height - 1000, 1000, 1000, 'purple');
+drawFilledRect(0, height - 1000, 1000, 1000, 'green');
 
 //drawCircle(width/2, height/4, 10, 'blue');
 
