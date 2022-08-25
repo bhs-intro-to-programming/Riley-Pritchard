971d76b04201546c19e3cf3d4042ab392d798ef0
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-08-25 15:20:29 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -7,7 +7,7 @@
 const drawFrame = (time) => {
   clear();
   drawFilledCircle((time / 3) % width, height / 2, 25, 'purple');
-  drawFilledCircle((time / 3) % width, height / 2, 25, 'blue');
+  drawFilledCircle((time / 4) % width, height / 2, 25, 'blue');
   drawFilledCircle((time / 3) % width, height / 2, 25, 'green');
   drawFilledCircle((time / 3) % width, height / 2, 25, 'yellow');
   drawFilledCircle((time / 3) % width, height / 2, 25, 'orange');
