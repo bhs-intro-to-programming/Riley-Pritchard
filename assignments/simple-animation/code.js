4ec92493fca48ab37d0fb645efa2de2266aa61cb
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-08-25 15:29:58 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -6,12 +6,12 @@
 // number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
-  drawFilledCircle((time / 2) % width/ 1.1, height / 2, 25, 'purple');
-  drawFilledCircle((time / 2.5) % width/ 1.1, height / 2, 25, 'blue');
-  drawFilledCircle((time / 3.1) % width/ 1.1, height / 2, 25, 'green');
-  drawFilledCircle((time / 3.9) % width/ 1.1, height / 2, 25, 'yellow');
-  drawFilledCircle((time / 4.9) % width/ 1.1, height / 2, 25, 'orange');
-  drawFilledCircle((time / 6.3) % width/ 1.1, height / 2, 25, 'red');
+  drawFilledCircle((time / 2) % width/ 1.1, height / 2, 25, 'grey');
+  drawFilledCircle((time / 2.5) % width/ 1.1, height / 2, 25, 'grey');
+  drawFilledCircle((time / 3.1) % width/ 1.1, height / 2, 25, 'grey');
+  drawFilledCircle((time / 3.9) % width/ 1.1, height / 2, 25, 'grey');
+  drawFilledCircle((time / 4.9) % width/ 1.1, height / 2, 25, 'grey');
+  drawFilledCircle((time / 6.3) % width/ 1.1, height / 2, 25, 'grey');
   drawFallingTriangle(width / 2, time);
 };
 
