5552b260880589c29a6e530385042f7383d381fc
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-08-25 15:27:31 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -38,7 +38,7 @@ const drawFallingTriangle = (x, time) => {
   let y3 = y1 - 13;
 
   // Actually draw the triangle.
-  drawTriangle(300, y1, x2, y2, x3, y3, 'grey', 6);
+  drawTriangle(300, y1, 100, y2, x3, y3, 'grey', 6);
 };
 
 // Leave this code here or the animation won't run. Also don't change the name
