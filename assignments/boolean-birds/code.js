c953dfb5770da074aea4a08f9cf4f922f3a1968c
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-06 15:28:13 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -16,6 +16,6 @@
  *   - Globby Birds eat fish, mice, and nuts.
  *
  */
-const isFlobbyBird = (isRed, isSpotted, eatsFish, eatsNuts, eatsWorms) => {
-  return isRed+isSpotted+eatsFish+eatsNuts+eatsWorms
+const isFlobbyBird = (isRed, isSpotted) => {
+  return isRed+isSpotted
 }
