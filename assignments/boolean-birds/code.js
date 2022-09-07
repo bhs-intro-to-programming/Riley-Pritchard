d98e180b3b4666b90636ec456dfe3a1f37917207
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-07 15:29:42 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -27,5 +27,5 @@ const eatsFish =(isRed, isSpotted) => !isRed || isSpotted
 const eatsMice =(isRed, isSpotted) => !isRed || !isSpotted
 
 const isRed =(s) => {
-  return s === 'flobby' || s === 'bloggy' || s !== 'flibble'
+  return s === 'flobby' || s === 'bloggy' || s !== 'flibble' || s !== 'globby'
 }
