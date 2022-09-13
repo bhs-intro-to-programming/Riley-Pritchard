5948f7130262ddab51f0473f127c3be3ebab4e01
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-13 15:23:58 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -25,7 +25,7 @@
  */
 
 const drawCircleLine = (x, y, r, color) => {
-  for (let n = 0; drawFilledCircle(x * n, y, r, color); n = n + 2)
+  for (let n = 0; drawFilledCircle(x + r * n, y, r, color); n = n + 2)
    //drawFilledCircle(x, y, r, color);
    //let width / (r * 2) = n
 //let drawFilledCircle2((x + r * 2), y, r, color)
