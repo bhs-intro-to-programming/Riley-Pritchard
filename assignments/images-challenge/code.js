e7c0c2378dd17882de84ab336c2f7bea6ac886bc
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-15 15:29:58 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -46,7 +46,7 @@ const drawSpicyCircleLine = (y, r, color) => {
 //drawSpicyCircleLine(280, 10, 'red')
 
 const drawSpicyCircles = (c, color) => {
-  for (let r = width / 2; c === c; r = r - width / (c / 4)) {
+  for (let r = width / 2; c === c; r = r - width / (c / 1.5)) {
     drawFilledCircle(width / 2, height / 2, r, color)
     if (color === 'red') {
       color = 'blue'
