29d236ef17ef29ac1ba48df8b5225777c0fa8f68
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-20 15:13:38 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -79,9 +79,9 @@ const drawSpicyCheckers = (n) => {
 
 const drawTooManyLines = (n) => {
   for (let x1 = 0; x1 < width; x1 = x1 + width / n) {
-    for (let y2 = 0; y2 < height; y2 = y2 + height / n) {
+    //for (let y2 = 0; y2 < height; y2 = y2 + height / n) {
       let y1 = 0
-      let x2 = width
+      //let x2 = width
       drawLine(x1, y1, x2, y2, 'black', 1)
     }
   }
