22b7518678ab629181206da9caf9377a3d700140
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-21 15:26:14 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -86,7 +86,7 @@ const drawTooManyLines = (n) => {
     let y2 = height
     let y1 = 0
     let x1 = 0
-    //drawLine(x1, y1, x2, y2, 'black', 1)
+    drawLine(x1, y1, x2, y2, 'black', 1)
   }
   for (let y1 = height; y1 > 0; y1 = y1 - height / n) {
     let x1 = 0
