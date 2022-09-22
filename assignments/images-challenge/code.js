6e2079d895806ec9a60b0cdf486fda7f43ebe28b
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-22 15:12:59 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -115,3 +115,13 @@ const drawSomeFunnyCircles = (r, p, color) => {
   }
 }
 //drawSomeFunnyCircles(17, .23, 'blue')
+
+const drawStrangePictureFrame = (r, color) => {
+  for (let y = r * 2; y < height - r * 2; y = y + r * 2) {
+    for (let n = 0; r * n < width - (r * 2); n = n + 2) {
+      let x = r * 2
+      drawCircle(x + r * n, y, r, color)
+    }
+  }
+}
+drawStrangePictureFrame
