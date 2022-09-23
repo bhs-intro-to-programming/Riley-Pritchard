370946e4ef6ed643332eefff32d54ac39a176da5
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-09-23 15:00:21 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -122,11 +122,12 @@ const drawStrangePictureFrame = (r, color) => {
       let x = r * 2
       if (n === 0) {
         drawCircle(x + r * n, y, r, color)
+      }
         if (y === height){
           drawCircle(x + r * n, y, r, color)
         }
       }
     }
   }
-}
+
 drawStrangePictureFrame(17, 'blue')
