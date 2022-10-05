82ab7257a877e8bfda133b30e90f7ff2adfa34a1
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-05 15:11:32 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -86,7 +86,7 @@ const countXs = (s) => {
 const countXXs = (s) => {
   let z = 0
   for (let y = 0; y < s.length + 1; y++) {
-    if (s[y].search(/[x]/)) {
+    if (s[y] = 'x') {
       z++
     } else {
       z = z
