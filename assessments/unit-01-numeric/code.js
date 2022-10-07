0200765b4b95136bdd2cbae9ebde216eb6505a02
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-07 14:58:00 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,38 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+//1 
+const itemsLeftOver = (items, people) => {
+  return items % people;
+}
+
+//2
+const areaOfCircle = (r) => {
+  return Math.PI * r**2;
+}
+
+//3
+const volumeOfCube = (l) => {
+  return l**3;
+}
+
+//4
+const populationGrowth = (population, growth) => {
+  return population * (growth + 1)
+}
+
+//5
+const earnedRunAverage = (eruns, innings) => {
+  return eruns / innings * 9
+}
+
+//6
+const valueOfJewels = (diamonds, emeralds, dvaluegold, evaluegold) => {
+  return (diamonds * dvaluegold) + (emeralds * evaluegold)
+}
+
+//7
+const firstClassPostage = (weight) => {
+  return 
+}
