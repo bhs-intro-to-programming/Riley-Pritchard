ddfb163446337afd10c91bc869efc68fe5b3fcb6
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-14 14:44:12 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,8 @@
 const countTens = [a]; {
-  for (let c = 0;c < countTens.length ; c++)
+  for (let c = 0; c < countTens.length; c++) {
     if (countTens[c] === 10) {
       tens++
     }
-    return tens
+  }
+  return tens
 }
