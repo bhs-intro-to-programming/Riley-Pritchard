af0dcf4bdf6eac098ba75491d21c89f1e6df58e4
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-17 15:14:29 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -34,3 +34,7 @@ const isLeapYear = (year) => {
 const firstAndLast = (s) => {
   return s[0] + s.substring(s.length - 1)
 }
+
+const swapFrontAndBack = (s) => {
+  return s.substring(s.length / 2) + s.substring(0, s.length / 2)
+}
