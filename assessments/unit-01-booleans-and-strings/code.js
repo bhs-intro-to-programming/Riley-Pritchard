dd49990ca75f340af0efdd01d7ee841940888181
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-17 15:16:48 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -38,3 +38,7 @@ const firstAndLast = (s) => {
 const swapFrontAndBack = (s) => {
   return s.substring(s.length / 2) + s.substring(0, s.length / 2)
 }
+
+const simplePigLatin = (s, n) => {
+  return s.substring(n) + s.substring(0, n) + 'ay'
+}
