77a5275446b10cbc78df330757a7ea043ef4c738
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-17 15:31:13 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -28,7 +28,7 @@ const evenGreaterThanZero = (x) => {
 
 //come back to 5
 const isLeapYear = (year) => {
-  return year % 4 === 0 && (year % 100 !== 0 && year % 400 === 0)
+  return year % 4 === 0 && !(year % 100 !== 0 && year % 400 === 0)
 }
   
 const firstAndLast = (s) => {
