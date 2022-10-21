6b6540170cf1c65013f6516f966b2417f23123e2
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-21 15:18:53 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -9,7 +9,7 @@
   //drawText('#', x, y, 'black', Math.min(width, height) * 0.3);
 //});
 
-drawText('#', 0, 0, 'black', 10)
+drawText('#', 0, 0, 'black', 200)
 
 const ticTacToe = () => {
   let board = []
