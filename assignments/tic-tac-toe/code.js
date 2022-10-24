943e2893908236a3513d00db48320e964a9e98f9
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-24 15:08:52 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -8,7 +8,7 @@
 registerOnclick((x, y) => {
   drawText('+', x, y, 'black', Math.min(width, height) * 0.3);
 });
-drawLine(0, 0, width, height, 'black', 30)
+drawLine(width / 3, 0, width / 3, height, 'black', 30)
 
 const topLeft = () => {
   return x < width / 3 && y < height / 3
