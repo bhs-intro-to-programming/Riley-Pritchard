1ea49888794456ccd0c85e620dbc2c614d6445d0
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-10-25 14:43:16 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,6 +6,7 @@
 // weeks but for now you can just adapt this code.
 
 registerOnclick((x, y) => {
+  if (text === 'X')
   drawText('X', x, y, 'black', Math.min(width, height) * 0.3);
 });
 drawLine(width / 3, 0, width / 3, height, 'black', 5)
