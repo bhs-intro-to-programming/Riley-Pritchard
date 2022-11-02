1b0936c8c135a7ca41f9608f360dbecfd52dddab
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-02 14:49:44 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -1,37 +1,37 @@
-const WHITE_KING   = '♔';
-const WHITE_QUEEN  = '♕';
-const WHITE_ROOK   = '♖';
+const WHITE_KING = '♔';
+const WHITE_QUEEN = '♕';
+const WHITE_ROOK = '♖';
 const WHITE_BISHOP = '♗';
 const WHITE_KNIGHT = '♘';
-const WHITE_PAWN   = '♙';
-const BLACK_KING   = '♚';
-const BLACK_QUEEN  = '♛';
-const BLACK_ROOK   = '♜';
+const WHITE_PAWN = '♙';
+const BLACK_KING = '♚';
+const BLACK_QUEEN = '♛';
+const BLACK_ROOK = '♜';
 const BLACK_BISHOP = '♝';
 const BLACK_KNIGHT = '♞';
-const BLACK_PAWN   = '♟';
+const BLACK_PAWN = '♟';
 
 // Example of drawing one of the pieces
 //drawText(WHITE_KING, width/2, height/2, 'black', 64);
 
 const drawBoard = () => {
-  for (let i = 0; i < 8; i++){
-  drawLine(width / 8 * i, 0, width / 8 * i, height, 'black', 2)
+  for (let i = 0; i < 8; i++) {
+    drawLine(width / 8 * i, 0, width / 8 * i, height, 'black', 2)
   }
-  for (let j = 0; j < 8; j++){
+  for (let j = 0; j < 8; j++) {
     drawLine(0, height / 8 * j, width, height / 8 * j, 'black', 2)
   }
 }
 
-let board = 
-[['wr','wn','wb','wk','wq','wb','wn','wr']
-['wp','wp','wp','wp','wp','wp','wp','wp']
-['','','','','','','','']
-['','','','','','','','']
-['','','','','','','','']
-['','','','','','','','']
-['bp','bp','bp','bp','bp','bp','bp','bp']
-['br','bn','bb','bk','bq','bb','bn','br']]
+let board =
+  [['wr', 'wn', 'wb', 'wk', 'wq', 'wb', 'wn', 'wr'],
+  ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['', '', '', '', '', '', '', ''],
+  ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+  ['br', 'bn', 'bb', 'bk', 'bq', 'bb', 'bn', 'br']]
 let player = 'w'
 
 const row = Math.floor(y / (height / 8))
