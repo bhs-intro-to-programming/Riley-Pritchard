a4a642e5a2c1342af000d720f5cbc829bf01c46c
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-02 14:44:08 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -14,13 +14,12 @@ const BLACK_PAWN   = '♟';
 // Example of drawing one of the pieces
 //drawText(WHITE_KING, width/2, height/2, 'black', 64);
 
-drawLine(width / 8, 0, width / 8, height, 'black', 2)
-drawLine(width / 4, 0, width / 4, height, 'black', 2)
-drawLine(width / 8 * 3, 0, width / 8 * 3, height, 'black', 2)
-drawLine(width / 2, 0, width / 2, height, 'black', 2)
-drawLine(width / 8 * 5, 0, width / 8 * 5, height, 'black', 2)
-drawLine(width / 4 * 3, 0, width / 4 * 3, height, 'black', 2)
-drawLine(width / 8 * 7, 0, width / 8 * 7, height, 'black', 2)
+const drawBoard = () => {
+  for (let i = 0; i < 8; i++)
+  drawLine(width / 8 * i, 0, width / 8 * i, height, 'black', 2)
+}
+
+
 drawLine(0, height / 8, width, height / 8, 'black', 2)
 drawLine(0, height / 4, width, height / 4, 'black', 2)
 drawLine(0, height / 8 * 3, width, height / 8 * 3, 'black', 2)
