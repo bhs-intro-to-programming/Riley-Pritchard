ae3fd93a10ecdd49ef7b2c87b9b5239905e12fc8
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-03 14:55:08 -0700

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -38,13 +38,13 @@ let board =
 const row = (y) => Math.floor(y / (height / 8))
 const column = (x) => Math.floor(x / (width / 8))
 
-const drawArray = (x, y) => {
+const drawArray = () => {
   for (let j = 0; i < board.length; i++)
     for (let i = 0; i < 8; i++) {
       if (board[j][i] !== '') {
-        drawText(WHITE_KING, x / 8 * (i + 1), y / 8 * (j + 1), 'black', 64);
+        drawText(WHITE_KING, width / 8 * (i + 1), height / 8 * (j + 1), 'black', 64);
       }
     }
 }
 
-drawArray
+drawArray()
