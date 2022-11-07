5ade84cfc311b00ff1bff4ec5754cc2a63d1845b
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-07 15:31:57 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -16,10 +16,10 @@ const distance = (p1, p2) => {
   return Math.sqrt(Math.abs(p1.x - p2.x)**2 + Math.abs(p1.y - p2.y)**2)
 }
 
-const midpoint = ({'x' : x1,'y': y1}, {'x': x2,'y': y2}) => {
+const midpoint = (p1, p2) => {
   let o = {'x' : 0, 'y' : 0}
-   o.x = ((x2 + x1) / 2)
-   o.y = ((y2 + y1) / 2)
+   o.x = ((p2.x + p1.x) / 2)
+   o.y = ((p2.y + p1.y) / 2)
    return o
 }
 
