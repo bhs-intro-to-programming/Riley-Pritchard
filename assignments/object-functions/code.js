7b005224fc8f3512d5a7f858b05920f9bd089d89
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-08 15:05:24 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -49,7 +49,7 @@ const newHighScore = (score, a) => {
 const summarizeBooks = (a) => {
   let o = {'titles' : [], 'pages' : 0}
   for (let i = 0; i < a.length; i++){
-    o.titles = {'titles' : [o.titles, a[i].titles]}
+    o.titles = {'titles' : [a[i].titles]}
     o.pages = o.pages + a[i].pages
   }
   return o
