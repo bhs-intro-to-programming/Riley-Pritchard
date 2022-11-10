15b836493109eca7bab7fca6ccf2cf252f61ec59
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-10 14:52:43 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,7 +1,9 @@
 const drawTheThings = () => {
   for (let i = 0; i < height; i++){
     for (let j = 0; j < width; j++){
+      if (Math.rand() < .5){
       drawFilledRect(i, j, 1, 1, 'yellow')
+      }
     }
   }
 }
