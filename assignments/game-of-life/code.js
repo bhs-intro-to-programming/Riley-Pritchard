9deb13826a2d26464727b06ea8a6310a9b2c1aac
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-15 15:03:26 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -15,26 +15,23 @@ const drawTheThings = () => {
 }
 drawTheThings()
 
-const whatLives = (start) => {
-  for (let i = 0; i < height; i =+ 5) {
-  for (start = 0; start < b.length; start++) {
+const whatLives = () => {
+  for (let i = 0; i < height; i += 5) {
+    for (let start = 0; start < b.length; start++) {
       if (b[start] > 0 && b[start + 1] > 0 && b[start + 3] > 0) {
         drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
         drawFilledRect(b[start] + 4, i, 5, 5, 'yellow')
         drawFilledRect(b[start] + 9, i, 5, 5, 'yellow')
         start = start + 2
-      }else{
+      } else {
         if (Math.random() < .2) {
-        drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
+          drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
         }
       }
     }
   }
-  for (a = b.length; a > -1; a = a - 1){
-    b.pop[a]
-  }
 }
-whatLives(start)
+whatLives()
 
 //animate = () => {
 //  drawTheThings
