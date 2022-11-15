da6cbf9833de200cce18e07791b1753f891de01f
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-15 15:11:39 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,39 +1,11 @@
 drawFilledRect(0, 0, width, height, 'black')
 
-
-
-const drawTheThings = () => {
-  for (let i = 0; i < height; i = i + 5) {
-    for (let j = 0; j < width; j = j + 5) {
-      if (Math.random() < .2) {
-        drawFilledRect(j, i, 5, 5, 'yellow')
-        b.push(j + 1)
+const board = () => {
+  let b = [[]]
+  for (let i = 0; i > width; i++) {
+    if  (Math.random() < .2) {
+        b.push(true)
       } else {
-        b.push(0)
-      }
-    }
+        b.push(false)
   }
 }
-drawTheThings()
-
-const whatLives = () => {
-  for (let i = 0; i < height; i += 5) {
-    for (let start = 0; start < b.length; start++) {
-      if (b[start] > 0 && b[start + 1] > 0 && b[start + 3] > 0) {
-        drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
-        drawFilledRect(b[start] + 4, i, 5, 5, 'yellow')
-        drawFilledRect(b[start] + 9, i, 5, 5, 'yellow')
-        start = start + 2
-      } else {
-        if (Math.random() < .2) {
-          drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
-        }
-      }
-    }
-  }
-}
-whatLives()
-
-//animate = () => {
-//  drawTheThings
-//}
