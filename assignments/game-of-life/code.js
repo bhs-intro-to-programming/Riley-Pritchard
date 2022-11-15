08438877b754a80651c94a62024a5afe2be564ee
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-15 15:11:48 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -3,9 +3,10 @@ drawFilledRect(0, 0, width, height, 'black')
 const board = () => {
   let b = [[]]
   for (let i = 0; i > width; i++) {
-    if  (Math.random() < .2) {
-        b.push(true)
-      } else {
-        b.push(false)
+    if (Math.random() < .2) {
+      b.push(true)
+    } else {
+      b.push(false)
+    }
   }
 }
