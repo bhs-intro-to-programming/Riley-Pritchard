7f5a9797ddfd04846fa7110e332eb025b170959a
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-16 15:12:08 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -43,7 +43,7 @@ const select = (x, y) => {
   drawText(board[row(y)][column(x)], width / 8 * column(x), height / 8 * (row(y) + 1), 'red', 40)
   aSelect.push(board[row(y)][column(x)])
       drawText(aSelect, width / 8 * column(x), height / 8 * (row(y) + 1), 'black', 40)
-    aSelect.pop
+    aSelect.pop(board[row(y)][column(x))
 }
 
 const move = (x, y) => {
