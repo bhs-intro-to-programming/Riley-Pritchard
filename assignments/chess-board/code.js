02633bdb23384485a73e766d383a29f1de99a04c
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-16 15:19:30 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -40,13 +40,15 @@ const column = (x) => Math.floor(x / (width / 8))
 
 let aSelect = []
 const select = (x, y) => {
-  drawText(board[row(y)][column(x)], width / 8 * column(x), height / 8 * (row(y) + 1), 'red', 40)
-  aSelect.push(board[row(y)][column(x)])
-  if (aSelect !== []){
-      drawText(aSelect, width / 8 * column(x), height / 8 * (row(y) + 1), 'black', 30)
+  if (aSelect !== []) {
+    drawText(aSelect, width / 8 * column(x), height / 8 * (row(y) + 1), 'black', 30)
     aSelect.pop(board[row(y)][column(x)])
+  } else {
+    drawText(board[row(y)][column(x)], width / 8 * column(x), height / 8 * (row(y) + 1), 'red', 40)
+    aSelect.push(board[row(y)][column(x)])
   }
 }
+
 
 registerOnclick(select);
 
