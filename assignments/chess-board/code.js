41446d67d8a4f04c91bedce002c39c2adf4674be
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-16 14:49:03 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -42,6 +42,7 @@ const select = (x, y) => {
   let aSelect = []
   drawText(board[row(y)][column(x)], width / 8 * column(x), height / 8 * (row(y) + 1), 'red', 45)
   aSelect.push(board[row(y)][column(x)])
+  return aSelect
 }
 
 registerOnclick(select);
