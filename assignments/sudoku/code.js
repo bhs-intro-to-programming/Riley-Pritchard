290792b235a2f4ee8ba8435661773f4492d913f2
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-29 14:46:25 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -24,13 +24,13 @@ const left = (x) => {
 const right = (x) => {
   return x > width / 4 * 3;
 }
-const topp = (x, y) => {
+const top = (x, y) => {
   return x > width / 4 && x < width / 4 * 3 && y < height / 2;
 }
 const bot = (x, y) => {
   return x > width / 4 && x < width / 4 * 3 && y > height / 2;
 }
 
-registerOnclick = () => {
+registerOnclick( () => {
   random2()
-}
+});
