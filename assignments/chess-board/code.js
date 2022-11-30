91b3b010152c2c129446929bf7100ac5aff2a18f
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-30 15:04:10 -0800

    Updating assignments/chess-board/code.js

--- assignments/chess-board/code.js
+++ assignments/chess-board/code.js
@@ -15,6 +15,7 @@ const BLACK_PAWN = '♟';
 //drawText(WHITE_KING, width/2, height/2, 'black', 64);
 const rowA = () => {
   for (let i = 0; i < width; i += width / 8) {
+    let color = 'black'
     drawFilledRect(i, height / 8, width / 8, height / 8, color)
     if (color === 'black') {
       color = 'white'
