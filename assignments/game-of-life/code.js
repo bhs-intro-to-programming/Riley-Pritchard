eecad819f8f518b557af4bcaea33983147d31d50
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-11-30 15:10:34 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -1,50 +1,53 @@
 drawFilledRect(0, 0, width, height, 'black')
-let b = []
+
+let b = makeArray();
 
 const makeArrayRow = () => {
   let row = []
-  for (let i = 0;i < width; i += 5){
+  for (let i = 0; i < width; i += 5) {
     if (Math.random() < .2) {
-        row.push(1)
-      } else {
-        row.push(0)
-      }
+      row.push(1)
+    } else {
+      row.push(0)
+    }
   }
   return row
 }
 const makeArray = () => {
+  let b = []
   for (let i = 0; i < height; i += 5) {
     b.push(makeArrayRow())
-    }
   }
-makeArray()
+  return b;
+}
 
 
-  const drawTheThings = () => {
-    for (let i = 0; i < height; i++) {
-      for (let j = 0; j < width; j++) {
-        if (b[i][j] === 1) {
-          drawFilledRect(j * 5, i * 5, 5, 5, 'yellow')
-        }
+
+const drawTheThings = () => {
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (b[i][j] === 1) {
+        drawFilledRect(j * 5, i * 5, 5, 5, 'yellow')
       }
     }
   }
-  drawTheThings()
+}
+drawTheThings()
 
-  const whatLives = () => {
-    for (let i = 0; i < height; i += 5) {
-      for (let start = 0; start < b.length; start++) {
-        if (b[start] > 0 && b[start + 1] > 0 && b[start + 3] > 0) {
+const whatLives = () => {
+  for (let i = 0; i < height; i += 5) {
+    for (let start = 0; start < b.length; start++) {
+      if (b[start] > 0 && b[start + 1] > 0 && b[start + 3] > 0) {
+        drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
+        drawFilledRect(b[start] + 4, i, 5, 5, 'yellow')
+        drawFilledRect(b[start] + 9, i, 5, 5, 'yellow')
+        start = start + 2
+      } else {
+        if (Math.random() < .2) {
           drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
-          drawFilledRect(b[start] + 4, i, 5, 5, 'yellow')
-          drawFilledRect(b[start] + 9, i, 5, 5, 'yellow')
-          start = start + 2
-        } else {
-          if (Math.random() < .2) {
-            drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
-          }
         }
       }
     }
   }
+}
 
