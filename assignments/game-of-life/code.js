d163f3f6242ff754650cdcafddcbffa70c09f2a9
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-01 14:55:56 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -23,7 +23,7 @@ const makeArray = (rows, columns) => {
 
 
 
-const drawTheThings = (b, cellsize) => {
+const drawTheThings = (b) => {
   for (let i = 0; i < b.length; i++) {
     for (let j = 0; j < b[i].length; j++) {
       if (b[i][j] === 1) {
