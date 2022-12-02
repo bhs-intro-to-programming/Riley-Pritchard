a857ad8e1bd0f33cec230ab53b9f2b656e028a44
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-02 15:14:01 -0800

    Updating assignments/game-of-life/code.js

--- assignments/game-of-life/code.js
+++ assignments/game-of-life/code.js
@@ -37,10 +37,6 @@ const whatLives = () => {
   for (let i = 0; i < height; i += 5) {
     for (let start = 0; start < b.length; start++) {
       if (b[start] > 0 && b[start + 1] > 0 && b[start + 3] > 0) {
-        drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
-        drawFilledRect(b[start] + 4, i, 5, 5, 'yellow')
-        drawFilledRect(b[start] + 9, i, 5, 5, 'yellow')
-        start = start + 2
       } else {
         if (Math.random() < .2) {
           drawFilledRect(b[start] - 1, i, 5, 5, 'yellow')
