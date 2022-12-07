ea9764b6fb75e7f63e8d2286e4075b3a34c614df
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-07 15:26:19 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -23,7 +23,7 @@ const random2 = () => {
   drawText('2', x, y, 'black', 80)
   board[row(y)][column(x)] = '2'
 }
-
+random2()
 
 const drawArray = () => {
   for (let j = 0; j < board.length; j++)
@@ -39,7 +39,6 @@ const doIt = () => {
   clear()
   drawBoard()
   drawArray()
-  random2()
 }
 
 const moveLeft = () => {
