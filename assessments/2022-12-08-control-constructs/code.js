8ffe48fd3546b91398ec4a9121c465057a1c9bd1
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-08 14:41:26 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -5,3 +5,12 @@ const yesIfEven = (x) => {
     return 'no'
   }
 }
+
+const countXs = (s) => {
+  let count = 0
+  for (let i = 0; i < s.length; i++){
+    if (s[i] === 'x'){
+      count = count + 1
+    }
+  }
+}
