8151c16d196bee4cf6e53e116d0241f80b8e8a35
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-08 14:53:19 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -1,15 +1,15 @@
 const yesIfEven = (x) => {
-  if (x % 2 === 0){
+  if (x % 2 === 0) {
     return 'yes'
-  }else{
+  } else {
     return 'no'
   }
 }
 
 const countXs = (s) => {
   let count = 0
-  for (let i = 0; i < s.length; i++){
-    if (s[i] === 'x'){
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 'x') {
       count = count + 1
     }
   }
@@ -22,17 +22,15 @@ const emit = (x, y, z) => {
 }
 
 const timesTable = (x) => {
-  for (let i = 1; i < x + 1; i++){
-    for (let j = 1; j < x + 1; j++){
+  for (let i = 1; i < x + 1; i++) {
+    for (let j = 1; j < x + 1; j++) {
       emit(i, j, i * j)
     }
   }
 }
 
 const containsX = (s) => {
-  for (let i = 0; i < s.length; i++){
-    if (s[i] === 'x'){
-      return true
-    }
+  for (let i = 0; i < s.length; i++) {
+    return s[i] === 'x'
   }
 }
