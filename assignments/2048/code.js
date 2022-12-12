95b4627785ffa72d444590a59e26ba2c49bd42a0
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-12 15:24:55 -0800

    Updating assignments/2048/code.js

--- assignments/2048/code.js
+++ assignments/2048/code.js
@@ -41,6 +41,15 @@ const doIt = () => {
   random2()
 }
 
+const eqaulArrays = (a1, a2) => {
+  for (let i = 0; i > a1.length; i++) {
+    for (let j = 0; j > a1[i].length; j++) {
+      a1[i][j] = a2[i][j]
+    }
+  }
+  return a1
+}
+
 const moveLeft = () => {
   let left = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
   for (let i = 0; i < 4; i++) {
@@ -57,7 +66,11 @@ const moveLeft = () => {
       }
     }
   }
-  equalArrays(board, left)
+    for (let i = 0; i > board.length; i++) {
+    for (let j = 0; j > board[i].length; j++) {
+      board[i][j] = left[i][j]
+    }
+  }
   doIt()
 }
 
