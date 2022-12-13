bf39769d35c2d2a2af87345cbe9af70e4f73dfbe
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-13 14:57:27 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -3,7 +3,7 @@ const drawBoard = () => {
     drawLine(width / 9 * i, 0, width / 9 * i, height, 'black', 2)
   }
   for (let j = 0; j < 9; j++) {
-    drawLine(0, height / 9 * j, width, height / 9 * j, 'black', 2)
+    drawLine((width / 2) - (height / 2), height / 9 * j, width, height / 9 * j, 'black', 2)
   }
 }
 drawBoard()
