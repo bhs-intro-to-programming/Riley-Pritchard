80004e2178c11c85f16bc3071836651441dc8a36
Riley-Pritchard <111394854+Riley-Pritchard@users.noreply.github.com>
2022-12-16 10:50:50 -0800

    Updating assignments/sudoku/code.js

--- assignments/sudoku/code.js
+++ assignments/sudoku/code.js
@@ -1,19 +1,9 @@
 const drawBoard = (thick) => {
   for (let i = 0; i < 10; i++) {
-    drawLine((width / 2) - (height / 2) + (height / 9) * i, 0, (width / 2) - (height / 2) + height / 9 * i, height, 'black', thick)
-    if (i % 3 === 0){
-      thick = 2;
-    }else{
-      thick = 1;
-    }
+    drawLine((width / 2) - (height / 2) + (height / 9) * i, 0, (width / 2) - (height / 2) + height / 9 * i, height, 'black', 1)
   }
   for (let j = 0; j < 10; j++) {
-    drawLine((width / 2) - (height / 2), height / 9 * j, (width / 2) + (height / 2), height / 9 * j, 'black', thick)
-  if (j % 3 === 2/3){
-      thick = 2;
-    }else{
-      thick = 1;
-    }
+    drawLine((width / 2) - (height / 2), height / 9 * j, (width / 2) + (height / 2), height / 9 * j, 'black', 1)
   }
 }
 drawBoard(1)
